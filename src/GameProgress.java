ca5d0fdfb21bc4ab26542c9b007cb1ee0391d2c8
Stanislav Efremov <efremovsb@mail.ru>
2024-04-02 16:29:17 +0300

    bug fixing

--- src/GameProgress.java
+++ src/GameProgress.java
@@ -18,47 +18,14 @@ public class GameProgress implements Serializable {
         this.distance = distance;
     }
 
-
-    public void saveGame(String dirSave, GameProgress gameProgress) {
-        try (FileOutputStream fos = new FileOutputStream(dirSave); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-            // запишем экземпляр класса в файл
-            oos.writeObject(gameProgress);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-
-    public void zipFiles(String dirZip, List<String> fileName) {
-        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(dirZip))) {
-
-            for (String flt : fileName) {
-                try (FileInputStream fis = new FileInputStream(flt)) {
-
-                    ZipEntry entry = new ZipEntry(flt);
-                    zout.putNextEntry(entry);
-
-                    byte[] buffer = new byte[fis.available()];
-                    fis.read(buffer);
-                    zout.write(buffer);
-                    zout.closeEntry();
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
-            }
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-        for (String flt : fileName) {
-            File deleteFile = new File(flt);
-            if (deleteFile.exists() || deleteFile.isFile()) {
-                deleteFile.delete();
-            }
-        }
-    }
-
     @Override
     public String toString() {
-        return "GameProgress{" + "health=" + health + ", weapons=" + weapons + ", lvl=" + lvl + ", distance=" + distance + '}';
+        return "GameProgress{" +
+                "health=" + health +
+                ", weapons=" + weapons +
+                ", lvl=" + lvl +
+                ", distance=" + distance +
+                '}';
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,55 @@
+import java.io.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Main {
     public static void main(String[] args) {
-        GameProgress gameProgress1 = new GameProgress(10, 2, 2, 2.5);
-        GameProgress gameProgress2 = new GameProgress(40, 4, 3, 3.8);
-        GameProgress gameProgress3 = new GameProgress(60, 5, 4, 4.2);
+        List<String> fileName = Arrays.asList("C:\\Games\\savegames\\data1.dat", "C:\\Games\\savegames\\data2.dat", "C:\\Games\\savegames\\data3.dat");
 
-        List<String> fileName = Arrays.asList ("C:\\Games\\savegames\\data1.dat","C:\\Games\\savegames\\data2.dat", "C:\\Games\\savegames\\data3.dat");
+        saveGame(fileName.get(0), new GameProgress(10, 2, 2, 2.5));
+        saveGame(fileName.get(1), new GameProgress(40, 4, 3, 3.8));
+        saveGame(fileName.get(2), new GameProgress(60, 5, 4, 4.2));
 
-        gameProgress1.saveGame(fileName.get(0),gameProgress1);
-        gameProgress2.saveGame(fileName.get(1),gameProgress2);
-        gameProgress3.saveGame(fileName.get(2),gameProgress3);
+        zipFiles("C:\\Games\\savegames\\zip.zip", fileName);
+    }
 
-        gameProgress1.zipFiles("C:\\Games\\savegames\\zip.zip", fileName);
+    public static void saveGame(String dirSave, GameProgress gameProgress) {
+        try (FileOutputStream fos = new FileOutputStream(dirSave); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            // запишем экземпляр класса в файл
+            oos.writeObject(gameProgress);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 
+    public static void zipFiles(String dirZip, List<String> fileName) {
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(dirZip))) {
+
+            for (String flt : fileName) {
+                try (FileInputStream fis = new FileInputStream(flt)) {
+
+                    ZipEntry entry = new ZipEntry(flt);
+                    zout.putNextEntry(entry);
+
+                    byte[] buffer = new byte[fis.available()];
+
+                    fis.read(buffer);
+                    zout.write(buffer);
+                    zout.closeEntry();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        for (String flt : fileName) {
+            File deleteFile = new File(flt);
+            if (deleteFile.exists() || deleteFile.isFile()) {
+                deleteFile.delete();
+            }
+        }
     }
 }
